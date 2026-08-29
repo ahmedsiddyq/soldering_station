5ce24b6017d26dd0d82c34b1b84f646717e0ace1
siddyq <131194450+ahmedsiddyq@users.noreply.github.com>
2026-08-29 11:49:21 +0300

    سوفت وير تايم

---
+++ lcd_ST/firmware/st_16f15223_irsegmet.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=st_16f15223_irsegmet.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/st_16f15223_irsegmet.X.production.hex
